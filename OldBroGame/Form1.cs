4adb54123acb122e995f435841061bcea3eb0ec9
Joseph McCollum <mccolluj7506@student.faytechcc.edu>
2023-05-17 18:22:42 -0400

    Added a new Character, fixed parts of the design

---
+++ OldBroGame/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OldBroGame
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void exitBtn_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void backBtn_Click(object sender, EventArgs e)
+        {
+            Form2 titlePage = new Form2();
+            this.Hide();
+            titlePage.Show();
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            string name = "Dutch Plissken";
+            const int AGE = 56;
+            label2.Text = ($"{name} , Age: {AGE}");
+            pictureBox1.Image = imageList1.Images[0];
+            label4.Text = ($"{name} : Born in Wales, UK.\nDutch learned through various fights growing up he was a talented street fighter.\nHe tried to turn his life over and enter professional boxing but certain mishaps happen.\nIllegal street boxing rings and arms dealing lead him behind bars.\nSince his release from prison Dutch turned a new leaf and chose to justify his wrongdoings.\nHis specialty is boxinb/grappling/submissions short/mid/long range weapons.");
+
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            string name = "Logan Kelly";
+            const int AGE = 44;
+            label2.Text = ($"{name} , Age: {AGE}");
+            pictureBox1.Image = imageList1.Images[1];
+            label4.Text = ($"{name} : Born in Alaska. \nLogan has a natural sense for the hunt and battle.\nAn outcast since the death of his parents at the age of 10 by a wild wolf pack.\nLogan went under the wing of his grandfather and has been trained to survive.\nAnd survival is what he knows best.\nHis specialty is Close quaters combat, long distance, hand crafted weapons and demoltions.");
+
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            string name = "Sergei Kozlov";
+            const int AGE = 36;
+            label2.Text = ($"{name} , Age: {AGE}");
+            pictureBox1.Image = imageList1.Images[2];
+            label4.Text = ($"{name} : Born in Moscow, Russia. \nInfluenced by his older now deceased brother to join a life of crime in his younger years.\nHe knows his ways around organized crime as well as the justice system.\nSergei has turned his life around and fights against the wrongs\n that he felt in his heart he committed even if he had no ties to it.\nHis specialty is mid/long distance weapons, hand to hand combat and steet smarts. ");
+
+        }
+
+        private void enterBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+            string name = "Misty X";
+            const int AGE = 23;
+            label2.Text = ($"{name} , Age: {AGE}");
+            pictureBox1.Image = imageList1.Images[3];
+            label4.Text = ($"{name} : Born in Parts Unknown. \nMisty's childhood is cloudy from her last full memory of waking up in a labratory.\nTrying to gain what little she has from her lost memory\n she notices that she is very skilled in all forms of combat and survival.\nSergei saves her and a few other lost children from the labratoy.\nShe joins with the OldBros to find out who she really is!\nAnd to stop terror in the city and save all that she can!");
+        }
+    }
+}
---
+++ OldBroGame/Form3.Designer.cs
@@ -0,0 +1,66 @@
+﻿
+namespace OldBroGame
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form3));
+            this.backBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // backBtn
+            // 
+            this.backBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.backBtn.Location = new System.Drawing.Point(1110, 700);
+            this.backBtn.Name = "backBtn";
+            this.backBtn.Size = new System.Drawing.Size(150, 50);
+            this.backBtn.TabIndex = 0;
+            this.backBtn.Text = "Back";
+            this.backBtn.UseVisualStyleBackColor = true;
+            this.backBtn.Click += new System.EventHandler(this.backBtn_Click);
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1378, 944);
+            this.Controls.Add(this.backBtn);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form3";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Game Description";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button backBtn;
+    }
+}
